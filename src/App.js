06b24671d10cfec32d718c1898673be11ab36ca6
Vital-jan <vitaljan@gmail.com>
2021-05-16 01:55:40 +0300

    numbers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+function NumberLarge (props) {
+    const deg = {
+        transform: `rotate(${props.deg}deg)`
+    }
+    const degReverse = {
+        transform: `rotate(-${props.deg}deg)`
+    }
+    return (
+        <div className="number lg" style={deg}>
+            <span style={degReverse}>{props.number}</span>
+        </div>
+    );
+}
+
+function NumberSmall (props) {
+    const deg = {
+        transform: `rotate(${props.deg}deg)`
+    }
+
+    return (
+        <div className="number sm" style={deg}></div>
+    );
+}
+
+function Circle() {
+    let smallNumberDeg = [30, 60, 120, 150, 210, 240, 300, 330];
+    let degArr = smallNumberDeg.map((val)=>{
+        return (<NumberSmall deg={val}/>);
+    })
+console.log(degArr)
+    return (
+        <div className="circle">
+            <img className="matterhorn" src="/img/matterhorn.png" alt="Matterhorn"></img>
+            <NumberLarge number="XII" deg="0"/>
+            <NumberLarge number="III" deg="90"/>
+            <NumberLarge number="VI" deg="180"/>
+            <NumberLarge number="IX" deg="270"/>
+            {degArr}
+        </div>
+    );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Circle/>
   );
 }
 
