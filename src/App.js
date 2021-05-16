230dadb1ae6063cd39a00075a9d10ac9063976f2
Vital-jan <vitaljan@gmail.com>
2021-05-16 22:38:04 +0300

    minute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-// import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function NumberLarge (props) {
@@ -25,10 +26,68 @@ function NumberSmall (props) {
     );
 }
 
+class ArrowSecond extends React.Component {
+    constructor (props) {
+        super(props);
+        let time = new Date();
+        this.state = {
+            angle: time.getSeconds() * 6,
+            style: {
+                transform: `rotate(0deg)`
+            }
+        };
+    }
+    componentDidMount () {
+        let interval = setInterval (()=>{
+            this.setState({angle: this.state.angle + 6});
+            if (this.state.angle > 360) this.setState({angle: 0});
+            this.setState({style: {transform: `rotate(${this.state.angle}deg)`}});
+            console.log(this.state.angle)
+        }, 1000);
+        }
+    setAngle (angle) {
+        this.setState({angle: angle});
+    }
+    render () {
+        return (
+            <div className="arrow arrow-s" style={this.state.style}></div>
+        );
+    }
+}
+
+class ArrowMinute extends React.Component {
+    constructor (props) {
+        super(props);
+        let time = new Date();
+        this.state = {
+            angle: time.getMinutes() * 6,
+            style: {
+                transform: `rotate(${time.getMinutes() * 6}deg)`
+            }
+        };
+    }
+    componentDidMount () {
+        let interval = setInterval (()=>{
+            this.setState({angle: this.state.angle + 6});
+            if (this.state.angle > 360) this.setState({angle: 0});
+            this.setState({style: {transform: `rotate(${this.state.angle}deg)`}})
+            console.log(this.state.angle)
+        }, 60000);
+        }
+    setAngle (angle) {
+        this.setState({angle: angle});
+    }
+    render () {
+        return (
+            <div className="arrow arrow-m" style={this.state.style}></div>
+        );
+    }
+}
+
 function Circle() {
     let smallNumberDeg = [30, 60, 120, 150, 210, 240, 300, 330];
     let degArr = smallNumberDeg.map((val)=>{
-        return (<NumberSmall deg={val}/>);
+        return (<NumberSmall deg={val} key={val}/>);
     })
 console.log(degArr)
     return (
@@ -39,14 +98,17 @@ console.log(degArr)
             <NumberLarge number="VI" deg="180"/>
             <NumberLarge number="IX" deg="270"/>
             {degArr}
+            <ArrowSecond/>
+            <ArrowMinute/>
         </div>
     );
 }
 
 function App() {
-  return (
-      <Circle/>
-  );
+
+    return (
+        <Circle/>
+    );
 }
 
 export default App;
