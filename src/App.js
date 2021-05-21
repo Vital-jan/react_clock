04341e33b25803e67f7bd9af836e5c3221a2fdd2
Vital-jan <vitaljan@gmail.com>
2021-05-21 19:45:31 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -26,28 +26,6 @@ function NumberSmall(props) { // other clock numbers
     );
 }
 
-// class Arrow extends React.Component { // clock arrow abstract class
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             style: null,
-//         };
-//     }
-
-//     refresh = 1000; // time to refresh (ms)
-
-//     move(time, force = false) { } // move clock arrow
-
-//     componentDidMount() {
-//         let time = new Date();
-//         this.move(time, true); // clock arrow start position
-//         let interval = setInterval(() => {
-//             let time = new Date();
-//             this.move(time); // move clock arrow
-//         }, this.refresh)
-//     }
-// }
-
 function ArrowSecond(props) {
     return (
         <div className="arrow arrow-s" style={props.style}></div>
@@ -63,72 +41,9 @@ function ArrowHour(props) {
         <div className="arrow arrow-h" style={props.style}></div>
     );
 }
-// class ArrowSecond extends Arrow {
-//     getAngle() {
-//         return 1000;
-//     }
-//     move(time) {
-//         let rotate = `rotate(${time.getSeconds() * 6}deg)`; // set arrow angle
-//         this.setState({
-//             style: {
-//                 transform: rotate
-//             }
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="arrow arrow-s" style={this.state.style}></div>
-//         );
-//     }
-// }
-
-// class ArrowMinute extends Arrow {
-
-//     move(time) {
-//         let rotate = `rotate(${time.getMinutes() * 6}deg)`; // set arrow angle
-//         this.setState({
-//             style: {
-//                 transform: rotate
-//             }
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="arrow arrow-m" style={this.state.style}></div>
-//         );
-//     }
-// }
-
-// class ArrowHour extends Arrow {
-
-//     refresh = 60000; // time to refresh hour arrow position
-
-//     move(time, force) {
-//         let rotate = `rotate(${time.getHours() * 30 + time.getMinutes() / 2}deg)`;
-//         this.setState({
-//             style: {
-//                 transform: rotate
-//             }
-//         });
-
-//         if (time.getMinutes() === 0 || force)
-//             this.setState({
-//                 style: {
-//                     transform: rotate
-//                 }
-//             });
-//     }
-
-//     render() {
-//         return (
-//             <div className="arrow arrow-h" style={this.state.style}></div>
-//         );
-//     }
-// }
 
 class Circle extends React.Component { // parent component
+    iterval;
     smallNumberDeg = [30, 60, 120, 150, 210, 240, 300, 330];
     degArr = [];
     state = {
@@ -173,7 +88,7 @@ class Circle extends React.Component { // parent component
         let time = new Date(); // start setup arrows position
         this.setMinuteArrow(time);
         this.setHourArrow(time);
-        setInterval(() => {
+        this.interval = setInterval(() => {
             let time = new Date();
             this.setState( // set second arrow position (1 time per second)
                 {
@@ -184,12 +99,15 @@ class Circle extends React.Component { // parent component
             if (time.getSeconds() === 0) { // set minute arrow position (1 time per minute)
                 this.setMinuteArrow(time);
             }
-            if (time.getSeconds() % 5 === 0) { // set hour arrow position (1 time per 5 minutes)
+            if (time.getMinutes() % 5 === 0) { // set hour arrow position (1 time per 5 minutes)
                 this.setHourArrow(time);
             }
         }, 1000);
     }
-
+    componentDidUnmount() {
+        clearInterval(this.interval);
+    }
+    
     render() {
         return (
             <div className="circle">
@@ -201,7 +119,7 @@ class Circle extends React.Component { // parent component
                 {this.degArr}
                 <ArrowSecond style={this.state.secondAngle} />
                 <ArrowMinute style={this.state.minuteAngle} />
-                <ArrowHour style={this.state.hourAngle}/>
+                <ArrowHour style={this.state.hourAngle} />
             </div>
         );
     }
